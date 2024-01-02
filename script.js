1a062705421605a90ac75e83d20804af42e560e1
Muhammed Enes GÜLTEKIN <m.enes.gultkn@gmail.com>
2024-01-02 15:07:05 +0100

    added iss and finished first part

--- script.js
+++ script.js
@@ -1,9 +1,81 @@
+const map = L.map("map", {
+  maxZoom: 17,
+  minZoom: 3,
+  maxBounds: [
+    [-90, -180], // Southwest coordinates
+    [90, 180],
+  ],
+}).setView([49.75863040315576, 6.6431523766027], 1);
+async function OTM() {
+  var OpenTopoMap = await L.tileLayer(
+    "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+    {
+      maxZoom: 17,
+      attribution:
+        'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+    }
+  ).addTo(map);
+}
+
+OTM();
+var southWest = L.latLng(-90, -180),
+  northEast = L.latLng(90, 180);
+bounds = L.latLngBounds(southWest, northEast);
+
+map.setMaxBounds(bounds);
+map.on("drag", function () {
+  map.panInsideBounds(bounds, { animate: false });
+});
+let firsttime = true;
+let iss_coordinates = [];
 const api_iss = "https://api.wheretheiss.at/v1/satellites/25544";
 
 async function get_iss() {
   const response = await fetch(api_iss);
   const data = await response.json();
-  console.log(data);
+  let { latitude, longitude } = data;
+  iss_coordinates.push([latitude, longitude]);
+  //main.innerHTML = `<h1>${latitude},${longitude}</h1>`;
+  let issUrl = "data/iss.png";
+
+  addMarker(latitude, longitude, issUrl);
 }
 
-get_iss();
+const main = document.getElementById("main");
+const button = document.getElementById("location_button");
+
+let marker;
+function addMarker(lat, lng, iconUrl) {
+  let issIcon = L.icon({
+    iconUrl: iconUrl,
+    className: "iss",
+    iconSize: [64, 64],
+    iconAnchor: [16, 0],
+  });
+  if (marker) {
+    marker.remove();
+  }
+  marker = L.marker([lat, lng], {
+    icon: issIcon,
+  }).addTo(map);
+  if (firsttime) {
+    map.setView([lat, lng], 10);
+    firsttime = false;
+  }
+}
+setInterval(get_iss, 1000);
+
+let polyline;
+function firstpolyline() {
+  if (polyline) {
+    polyline.remove();
+  }
+  polyline = L.polyline(iss_coordinates, {
+    color: "red",
+    weight: 3,
+    opacity: 0.5,
+    smoothFactor: 1,
+  }).addTo(map);
+}
+
+setInterval(firstpolyline, 1000);
