b08c5abe662e32da765be52acf90ad231c979172
Muhammed Enes GÜLTEKIN <m.enes.gultkn@gmail.com>
2024-01-02 22:19:22 +0100

    iss and distance added

--- script.js
+++ script.js
@@ -34,23 +34,25 @@ async function get_iss() {
   const response = await fetch(api_iss);
   const data = await response.json();
   let { latitude, longitude } = data;
+  let lat = data.latitude;
+  let lon = data.longitude;
   iss_coordinates.push([latitude, longitude]);
   //main.innerHTML = `<h1>${latitude},${longitude}</h1>`;
   let issUrl = "data/iss.png";
 
   addMarker(latitude, longitude, issUrl);
+  //console.log(lat, lon);
+  return { lat, lon };
 }
 
-const main = document.getElementById("main");
-const button = document.getElementById("location_button");
-
+//create custom marker
 let marker;
 function addMarker(lat, lng, iconUrl) {
   let issIcon = L.icon({
     iconUrl: iconUrl,
     className: "iss",
     iconSize: [64, 64],
-    iconAnchor: [16, 0],
+    iconAnchor: [32, 64],
   });
   if (marker) {
     marker.remove();
@@ -62,9 +64,12 @@ function addMarker(lat, lng, iconUrl) {
     map.setView([lat, lng], 10);
     firsttime = false;
   }
+  clickMarker(marker, lat, lng);
 }
 setInterval(get_iss, 1000);
 
+let dist = [];
+//create polyline behind iss
 let polyline;
 function firstpolyline() {
   if (polyline) {
@@ -76,6 +81,27 @@ function firstpolyline() {
     opacity: 0.5,
     smoothFactor: 1,
   }).addTo(map);
-}
+  let distance = map.distance(
+    iss_coordinates[iss_coordinates.length - 1],
+    iss_coordinates[iss_coordinates.length - 2]
+  );
 
+  dist.push([dist.length + 1, distance]);
+}
 setInterval(firstpolyline, 1000);
+
+//event when click on marker
+function clickMarker(marker, lat, lng) {
+  marker.on("click", () => {
+    console.log(lat, lng);
+    map.flyTo([lat, lng]);
+  });
+}
+// let dist = [];
+// function distance(multipoint) {
+//   for (i in multipoint.length) {
+//     let d = map.distance(multipoint[i], multipoint[i - 1]);
+//     dist.push([i, d]);
+//   }
+//   return dist;
+// }
